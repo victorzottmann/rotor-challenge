9ee54a04838e9ca07955b1fb87df75a4a69327f2
Victor Zottmann <zottmann.victor@gmail.com>
2024-06-09 17:46:23 +1000

    style: add styling to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  gap: 4rem;
+`;
+
 const App = () => {
   return (
-    <h1>Hello World</h1>
+    <AppContainer>
+      <h1>Hello World</h1>
+    </AppContainer>
   )
 }
 
